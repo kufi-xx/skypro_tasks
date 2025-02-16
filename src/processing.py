2f4d056c7ccc5776dca074968ae01771c0a4b245
Вадим Гатцу <hahahalolka1@gmail.com>
2025-02-16 23:57:27 +0500

    filter/sort function

---
+++ src/processing.py
@@ -0,0 +1,13 @@
+from typing import Dict, List
+
+
+def filter_by_state(transactions, state="EXECUTED"):
+    """Фильтрует список словарей по значению ключа state"""
+    return [
+        transaction for transaction in transactions if transaction.get("state") == state
+    ]
+
+
+def sort_by_date(transactions: List[Dict], descending: bool = True) -> List[Dict]:
+    """Сортирует список словарей по дате"""
+    return sorted(transactions, key=lambda x: x["date"], reverse=descending)
